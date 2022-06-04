134f823b2c95d21ec75faa301e65b48af9a2c1c3
HankS06 <79720327+HankS06@users.noreply.github.com>
2022-06-04 10:16:55 -0400

    added turtle class

---
+++ Turtle.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Turtle here.
+ * 
+ * @author Hank 
+ * @version June 2022
+ */
+public class Turtle extends Actor
+{
+    /**
+     * Act - do whatever the Turtle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
+    }
+}
